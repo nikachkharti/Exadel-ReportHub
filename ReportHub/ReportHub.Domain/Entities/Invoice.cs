1f733fc24b2b94830534470e55f3abefd74780d7
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2025-03-24 19:54:40 +0400

    Invoice entitiy

---
+++ ReportHub/ReportHub.Domain/Entities/Invoice.cs
@@ -0,0 +1,12 @@
+﻿namespace ReportHub.Domain.Entities
+{
+    public class Invoice
+    {
+        public string InvoiceId { get; set; }
+        public DateTime IssueDate { get; set; }
+        public DateTime DueDate { get; set; }
+        public decimal Amount { get; set; }
+        public string Currency { get; set; }
+        public string PaymentStatus { get; set; }
+    }
+}
