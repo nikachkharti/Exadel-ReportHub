2b0e51d34b0a334ed47762998d21466ed6a884b5
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2025-03-25 21:39:05 +0400

    Test cases removed

--- ReportHub/ReportHub.Domain/Entities/Invoice.cs
+++ ReportHub/ReportHub.Domain/Entities/Invoice.cs
@@ -8,19 +8,11 @@ namespace ReportHub.Domain.Entities
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
-
-        //Ensure the format of InvoiceId follows this pattern after inserting of new value => 2025(Current year)001 2025(Current year)002 2025(Current year)003
         public string InvoiceId { get; set; }
         public DateTime IssueDate { get; set; }
-
-        //Ensure that due date is always greater than IssueDate
         public DateTime DueDate { get; set; }
         public decimal Amount { get; set; }
-
-        //Ensure that currency length is 3
         public string Currency { get; set; }
-
-        //Ensure that payment status is either Paid Pending or Overdue
         public string PaymentStatus { get; set; }
     }
 }
