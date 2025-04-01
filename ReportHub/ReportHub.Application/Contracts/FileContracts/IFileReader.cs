ab2069ecd9833267777347cb6854a653786c8140
boburjon1504 <boburjon67joraboyev@gmail.com>
2025-04-02 00:41:21 +0500

    added xml coomments

--- ReportHub/ReportHub.Application/Contracts/FileContracts/IFileReader.cs
+++ ReportHub/ReportHub.Application/Contracts/FileContracts/IFileReader.cs
@@ -2,5 +2,12 @@
 
 public interface IFileReader
 {
+    /// <summary>
+    /// Takes opened stream as parameter and read all data and return it as IEnumberable collection
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="stream"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
     Task<IEnumerable<T>> ReadAllAsync<T>(Stream stream, CancellationToken cancellationToken) where T : class;
 }
--- ReportHub/ReportHub.Application/Contracts/FileContracts/IFileWriter.cs
+++ ReportHub/ReportHub.Application/Contracts/FileContracts/IFileWriter.cs
@@ -1,8 +1,14 @@
-﻿using System.Collections.Generic;
-
-namespace ReportHub.Application.Contracts.FileContracts;
+﻿namespace ReportHub.Application.Contracts.FileContracts;
 
 public interface IFileWriter
 {
+    /// <summary>
+    /// Takes collection of data and statistics and writes it to a stream
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="datas"></param>
+    /// <param name="statistics"></param>
+    /// <param name="token"></param>
+    /// <returns></returns>
     Task<Stream> WriteAllAsync<T>(IEnumerable<T> datas, IReadOnlyDictionary<string, object> statistics,CancellationToken token);
 }
