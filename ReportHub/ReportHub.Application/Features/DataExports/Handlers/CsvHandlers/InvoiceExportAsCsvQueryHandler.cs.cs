cd0cf257095ae517f681cdb06f8d70b41605011e
boburjon1504 <boburjon67joraboyev@gmail.com>
2025-04-02 16:35:01 +0500

    `InvoiceExportAsCsvQueryHandler` was implemented with v1

--- ReportHub/ReportHub.Application/Features/DataExports/Handlers/CsvHandlers/InvoiceExportAsCsvQueryHandler.cs.cs
+++ ReportHub/ReportHub.Application/Features/DataExports/Handlers/CsvHandlers/InvoiceExportAsCsvQueryHandler.cs.cs
@@ -1,12 +1,34 @@
 ﻿using MediatR;
+using ReportHub.Application.Contracts;
+using ReportHub.Application.Contracts.FileContracts;
 using ReportHub.Application.Features.DataExports.Queries.CsvQueries;
 
 namespace ReportHub.Application.Features.DataExports.Handlers.CsvHandlers;
 
 public class InvoiceExportAsCsvQueryHandler : IRequestHandler<InvoiceExportAsCsvQuery, Stream>
 {
-    public Task<Stream> Handle(InvoiceExportAsCsvQuery request, CancellationToken cancellationToken)
+    private readonly IInvoiceRepository _invoiceRepository;
+    private readonly ICsvService _csvService;   
+    public InvoiceExportAsCsvQueryHandler(IInvoiceRepository invoiceRepository, ICsvService csvService)
     {
-        throw new NotImplementedException();
+        _invoiceRepository = invoiceRepository;
+        _csvService = csvService;
+    }
+    public async Task<Stream> Handle(InvoiceExportAsCsvQuery request, CancellationToken cancellationToken)
+    {
+        var invoices = await _invoiceRepository.GetAll(cancellationToken);
+        
+        return await _csvService.WriteAllAsync(invoices, GetSummary(invoices), cancellationToken);
+    }
+
+    private IReadOnlyDictionary<string, object> GetSummary(IEnumerable<Domain.Entities.Invoice> invoices)
+    {
+        return new Dictionary<string, object>
+        {
+            { "TotalInvoices", invoices.Count() },
+            { "Paid" , invoices.Where(x => x.PaymentStatus.Equals("Paid")) },
+            { "Pending", invoices.Where(x => x.PaymentStatus.Equals("Pending")) },
+            { "Overdue", invoices.Where(x => x.PaymentStatus.Equals("Overdue")) }
+        };
     }
 }
