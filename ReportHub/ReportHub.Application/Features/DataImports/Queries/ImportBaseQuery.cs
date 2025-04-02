0e3ef227726e51f45735e9bc7a280309972deb7c
boburjon1504 <boburjon67joraboyev@gmail.com>
2025-04-02 15:01:42 +0500

    added base query for import as abstract record `ImportBaseQuery<T>`

---
+++ ReportHub/ReportHub.Application/Features/DataImports/Queries/ImportBaseQuery.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace ReportHub.Application.Features.DataImports.Queries;
+
+public abstract record ImportBaseQuery<T>(Stream Stream, string FileExtension) : IRequest<IEnumerable<T>> where T : class;
