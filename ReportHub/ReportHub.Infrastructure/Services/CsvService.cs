74a34059c780da74d71168edfeb9edb4863ab98e
boburjon1504 <boburjon67joraboyev@gmail.com>
2025-04-02 15:54:48 +0500

    added `CsvService` class without implementation

---
+++ ReportHub/ReportHub.Infrastructure/Services/CsvService.cs
@@ -0,0 +1,16 @@
+﻿using ReportHub.Application.Contracts.FileContracts;
+
+namespace ReportHub.Infrastructure.Services;
+
+public class CsvService : ICsvService
+{
+    public Task<IEnumerable<T>> ReadAllAsync<T>(Stream stream, CancellationToken cancellationToken) where T : class
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Stream> WriteAllAsync<T>(IEnumerable<T> datas, CancellationToken token)
+    {
+        throw new NotImplementedException();
+    }
+}
