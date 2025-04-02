bb9778243759abd99a66d569f7dba7be5d8cfe6d
boburjon1504 <boburjon67joraboyev@gmail.com>
2025-04-02 15:46:58 +0500

    Added `InvoiceImportAsCsvQueryValidator` validator

---
+++ ReportHub/ReportHub.Application/Validators/DataImportQueryValidators/CsvQueryValidators/ImportQueryAsCsvQueryBaseValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using ReportHub.Application.Features.DataImports.Queries;
+
+namespace ReportHub.Application.Validators.DataImportQueryValidators.CsvQueryValidators;
+
+public class ImportQueryAsCsvQueryBaseValidator<TQuery, TType> : AbstractValidator<TQuery> 
+    where TQuery : ImportBaseQuery<TType> where TType : class
+{
+    public ImportQueryAsCsvQueryBaseValidator()
+    {
+        RuleFor(x => x.Stream)
+            .NotNull()
+            .WithMessage("File is required");
+
+        RuleFor(x => x.FileExtension)
+            .Equal(".csv")
+            .WithMessage("File extension is required");
+    }
+}
---
+++ ReportHub/ReportHub.Application/Validators/DataImportQueryValidators/CsvQueryValidators/InvoiceImportAsCsvQueryValidator.cs
@@ -0,0 +1,6 @@
+﻿using ReportHub.Application.Features.DataImports.Queries.CsvQueries;
+using ReportHub.Domain.Entities;
+
+namespace ReportHub.Application.Validators.DataImportQueryValidators.CsvQueryValidators;
+
+public class InvoiceImportAsCsvQueryValidator : ImportQueryAsCsvQueryBaseValidator<InvoiceImportAsCsvQuery, Invoice>{}
