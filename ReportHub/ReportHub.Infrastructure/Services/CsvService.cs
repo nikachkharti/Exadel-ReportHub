1e361e6c2dd1c8352e8dcb8660f46cbb6d77ee9e
boburjon1504 <boburjon67joraboyev@gmail.com>
2025-04-02 16:06:35 +0500

    Read logic implemented for `CsvService`

--- ReportHub/ReportHub.Infrastructure/Services/CsvService.cs
+++ ReportHub/ReportHub.Infrastructure/Services/CsvService.cs
@@ -1,15 +1,30 @@
-﻿using ReportHub.Application.Contracts.FileContracts;
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using ReportHub.Application.Contracts.FileContracts;
+using System.Globalization;
 
 namespace ReportHub.Infrastructure.Services;
 
 public class CsvService : ICsvService
 {
-    public Task<IEnumerable<T>> ReadAllAsync<T>(Stream stream, CancellationToken cancellationToken) where T : class
+    public async Task<IEnumerable<T>> ReadAllAsync<T>(Stream stream, CancellationToken cancellationToken) where T : class
     {
-        throw new NotImplementedException();
+        var streamReader = new StreamReader(stream);
+        var csvReader = new CsvReader(streamReader, CultureInfo.InvariantCulture);
+        var records = csvReader.GetRecordsAsync<T>(cancellationToken);
+
+        var result = new List<T>();
+        await foreach(var record in records)
+        {
+            if (record is null) break;
+
+            result.Add(record);   
+        }
+
+        return result;
     }
 
-    public Task<Stream> WriteAllAsync<T>(IEnumerable<T> datas, CancellationToken token)
+    public Task<Stream> WriteAllAsync<T>(IEnumerable<T> datas, IReadOnlyDictionary<string, object> statistics, CancellationToken token)
     {
         throw new NotImplementedException();
     }
