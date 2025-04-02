cec06b207fb6975068b3a54983dfd318333a0ddd
boburjon1504 <boburjon67joraboyev@gmail.com>
2025-04-02 15:28:11 +0500

    Added `InvoiceExportAsCsvQueryHandler` without implementation

---
+++ ReportHub/ReportHub.Application/Features/DataExports/Handlers/CsvHandlers/InvoiceExportAsCsvQueryHandler.cs.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using ReportHub.Application.Features.DataExports.Queries.CsvQueries;
+
+namespace ReportHub.Application.Features.DataExports.Handlers.CsvHandlers;
+
+public class InvoiceExportAsCsvQueryHandler : IRequestHandler<InvoiceExportAsCsvQuery, Stream>
+{
+    public Task<Stream> Handle(InvoiceExportAsCsvQuery request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
