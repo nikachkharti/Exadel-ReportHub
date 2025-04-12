4f46797f44c97420758347abbed7dc1cc7b4a4e9
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2025-04-12 23:07:16 +0400

    New custom exceptions added

---
+++ ReportHub/ReportHub.Application/Validators/Exceptions/NotFoundException.cs
@@ -0,0 +1,13 @@
+﻿namespace ReportHub.Application.Validators.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string message) : base(message)
+        {
+        }
+
+        public NotFoundException(string name, object key) : base($"Entity \"{name}\" ({key}) not found.")
+        {
+        }
+    }
+}
