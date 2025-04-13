b98e02d1956231a068d6e7c4e78234694eaca753
boburjon1504 <boburjon67joraboyev@gmail.com>
2025-04-13 12:47:17 +0500

    added `ClientUser` to Domain layer

---
+++ ReportHub/ReportHub.Domain/Entities/ClientUser.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ReportHub.Domain.Entities;
+
+public class ClientUser
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
+
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string ClientId { get; set; }
+
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string UserId { get; set; }
+
+    public string Role { get; set; }
+}
