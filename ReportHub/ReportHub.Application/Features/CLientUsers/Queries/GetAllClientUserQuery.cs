078fe811f2efbf00176e66d3ee46779c7b52a8c6
boburjon1504 <boburjon67joraboyev@gmail.com>
2025-04-13 15:22:07 +0500

    added `GetAllClientUserQuery` and `GetAllClientUserQueryHandler`

---
+++ ReportHub/ReportHub.Application/Features/CLientUsers/Queries/GetAllClientUserQuery.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using ReportHub.Domain.Entities;
+
+namespace ReportHub.Application.Features.CLientUsers.Queries;
+
+public record GetAllClientUserQuery : IRequest<IEnumerable<ClientUser>>;
