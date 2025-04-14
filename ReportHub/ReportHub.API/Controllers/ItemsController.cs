93c22304bc9d3dfb6691b797175bb0b0ace750d9
boburjon1504 <boburjon67joraboyev@gmail.com>
2025-04-14 11:35:55 +0500

    create item endpoint add

--- ReportHub/ReportHub.API/Controllers/ItemsController.cs
+++ ReportHub/ReportHub.API/Controllers/ItemsController.cs
@@ -1,11 +1,15 @@
 ﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using ReportHub.Application.Features.Item.Commands;
 using ReportHub.Application.Features.Item.Queries;
+using ReportHub.Application.Validators.Exceptions;
 using Serilog;
 using System.ComponentModel.DataAnnotations;
 
 namespace ReportHub.API.Controllers
 {
+    [Authorize(Roles = "SuperAdmin, Admin, ClientAdmin")]
     [Route("api/[controller]")]
     [ApiController]
     public class ItemsController(IMediator mediator) : ControllerBase
@@ -33,5 +37,26 @@ namespace ReportHub.API.Controllers
                 return StatusCode(500, ex.Message);
             }
         }
+        [HttpPost]
+        public async Task<IActionResult> ActionResult([FromBody] CreateItemCommand command)
+        {
+            try
+            {
+                Log.Information("Creating a new item.");
+                var itemId = await mediator.Send(command);
+                return CreatedAtAction(nameof(GetSingleItem), new { id = itemId }, itemId);
+            }
+            catch (InputValidationException ex)
+            {
+                Log.Error(ex, ex.Message);
+                return BadRequest(new { errorMessage = ex.Message, errors = ex.Errors });
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, ex.Message);
+                return StatusCode(500, ex.Message);
+            }
+        }
+
     }
 }
