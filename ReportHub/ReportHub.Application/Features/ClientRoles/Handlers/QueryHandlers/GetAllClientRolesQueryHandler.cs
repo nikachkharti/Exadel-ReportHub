faf37270dbf40311a7d25362ed5337eb674b4d04
boburjon1504 <boburjon67joraboyev@gmail.com>
2025-04-17 14:14:15 +0500

    added `GetAllClientRolesQuery` and its handler

---
+++ ReportHub/ReportHub.Application/Features/ClientRoles/Handlers/QueryHandlers/GetAllClientRolesQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using ReportHub.Application.Contracts.RepositoryContracts;
+using ReportHub.Application.Features.ClientRoles.Queries;
+using ReportHub.Domain.Entities;
+
+namespace ReportHub.Application.Features.ClientRoles.Handlers.QueryHandlers;
+
+public class GetAllClientRolesQueryHandler : IRequestHandler<GetAllClientRolesQuery, IEnumerable<ClientRole>>
+{
+    private readonly IClientRoleRepository _clientRoleRepository;
+
+    public GetAllClientRolesQueryHandler(IClientRoleRepository clientRoleRepository)
+    {
+        _clientRoleRepository = clientRoleRepository;
+    }
+
+    public Task<IEnumerable<ClientRole>> Handle(GetAllClientRolesQuery request, CancellationToken cancellationToken)
+    {
+        return _clientRoleRepository.GetAll(cancellationToken);
+    }
+}
