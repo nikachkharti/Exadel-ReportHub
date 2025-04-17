a630f00f65563d33099a66df403ff923a62e5de8
boburjon1504 <boburjon67joraboyev@gmail.com>
2025-04-17 14:18:32 +0500

    added `ClientRolesController`

---
+++ ReportHub/ReportHub.API/Controllers/ClientRolesController.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ReportHub.Application.Features.ClientRoles.Commands;
+using ReportHub.Application.Features.ClientRoles.Queries;
+
+namespace ReportHub.API.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ClientRolesController(IMediator mediator) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get(CancellationToken cancellationToken) 
+        {
+            var clientRoles = await mediator.Send(new GetAllClientRolesQuery(), cancellationToken);
+
+            if(clientRoles is null || !clientRoles.Any())
+            {
+                return NoContent();
+            }
+
+            return Ok(clientRoles);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateClientRoleCommand command, CancellationToken cancellationToken)
+        {
+            var clientRole = await mediator.Send(command, cancellationToken);
+
+            return Ok(clientRole);
+        }
+    }
+}
