616f5813c7e1b2b38d7098ba058832e65c97764c
boburjon1504 <boburjon67joraboyev@gmail.com>
2025-04-22 22:04:33 +0500

    added `DataSeeder` for seed

---
+++ ReportHub/ReportHub.Infrastructure/Workers/DataSeeder.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace ReportHub.Infrastructure.Workers;
+
+public class DataSeeder : IHostedService
+{
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
