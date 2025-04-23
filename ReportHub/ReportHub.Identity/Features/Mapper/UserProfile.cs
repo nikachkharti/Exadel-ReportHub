2f7bbfefea7e07f31e1d6cd66d23df56e93100d0
boburjon1504 <boburjon67joraboyev@gmail.com>
2025-04-24 00:00:49 +0500

    added mapper for user

---
+++ ReportHub/ReportHub.Identity/Features/Mapper/UserProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using ReportHub.Identity.Features.Users.Commands;
+using ReportHub.Identity.Models;
+
+namespace ReportHub.Identity.Features.Mapper;
+
+public class UserProfile : Profile
+{
+    public UserProfile()
+    {
+        CreateMap<CreateUserCommand, User>();
+    }
+}
