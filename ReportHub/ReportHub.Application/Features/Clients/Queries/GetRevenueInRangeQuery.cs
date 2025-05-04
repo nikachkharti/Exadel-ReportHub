3ac0e8849e03de5d60e91e53bc66d3bcf2a4ccf9
Dimitri <kenwo.dm@gmail.com>
2025-05-04 18:38:23 +0400

    add revenue in range query handler

---
+++ ReportHub/ReportHub.Application/Features/Clients/Queries/GetRevenueInRangeQuery.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using ReportHub.Application.Features.Clients.DTOs;
+
+namespace ReportHub.Application.Features.Clients.Queries;
+
+public record GetRevenueInRangeQuery(DateTime FromDate, DateTime ToDate, string CurrencyCode) : IRequest<RevenueDto>;
