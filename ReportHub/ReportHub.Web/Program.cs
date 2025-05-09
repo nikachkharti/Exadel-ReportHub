756ffdb87a3da160ab2bbcb0e50ddf08cc3cf31b
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2025-05-09 12:16:13 +0400

    Project init

---
+++ ReportHub/ReportHub.Web/Program.cs
@@ -0,0 +1,33 @@
+using ReportHub.Web.Components;
+
+namespace ReportHub.Web
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
+
+            var app = builder.Build();
+
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAntiforgery();
+
+            app.MapStaticAssets();
+            app.MapRazorComponents<App>()
+                .AddInteractiveServerRenderMode();
+
+            app.Run();
+        }
+    }
+}
